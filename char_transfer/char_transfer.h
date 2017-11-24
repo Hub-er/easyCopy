207b577ef0020b3a24e61af1c857844b75f3a85b
Hub-er <cloudroute@163.com>
2017-11-24 15:55:35 +0800

    Create char_transfer.h

---
+++ char_transfer/char_transfer.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#ifdef WIN32
+#include <tchar.h>
+#include "windows.h"
+
+static wchar_t * ANSIToUnicode(const char* str)
+{
+	int textlen;
+	wchar_t * result;
+	textlen = MultiByteToWideChar(CP_ACP, 0, str, -1, NULL, 0);
+	result = (wchar_t *)malloc((textlen + 1)*sizeof(wchar_t));
+	memset(result, 0, (textlen + 1)*sizeof(wchar_t));
+	MultiByteToWideChar(CP_ACP, 0, str, -1, (LPWSTR)result, textlen);
+	return result;
+}
+
+static char * UnicodeToANSI(const wchar_t* str)
+{
+	char* result;
+	int textlen;
+	textlen = WideCharToMultiByte(CP_ACP, 0, str, -1, NULL, 0, NULL, NULL);
+	result = (char *)malloc((textlen + 1)*sizeof(char));
+	memset(result, 0, sizeof(char)* (textlen + 1));
+	WideCharToMultiByte(CP_ACP, 0, str, -1, result, textlen, NULL, NULL);
+	return result;
+}
+
+static wchar_t * UTF8ToUnicode(const char* str)
+{
+	int textlen;
+	wchar_t * result;
+	textlen = MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL, 0);
+	result = (wchar_t *)malloc((textlen + 1)*sizeof(wchar_t));
+	memset(result, 0, (textlen + 1)*sizeof(wchar_t));
+	MultiByteToWideChar(CP_UTF8, 0, str, -1, (LPWSTR)result, textlen);
+	return result;
+}
+
+static char * UnicodeToUTF8(const wchar_t* str)
+{
+	char* result;
+	int textlen;
+	textlen = WideCharToMultiByte(CP_UTF8, 0, str, -1, NULL, 0, NULL, NULL);
+	result = (char *)malloc((textlen + 1)*sizeof(char));
+	memset(result, 0, sizeof(char)* (textlen + 1));
+	WideCharToMultiByte(CP_UTF8, 0, str, -1, result, textlen, NULL, NULL);
+	return result;
+}
+
+static char* ANSIToUTF8(const char* str)
+{
+	return UnicodeToUTF8(ANSIToUnicode(str));
+}
+
+static char* UTF8ToANSI(const char* str)
+{
+	return UnicodeToANSI(UTF8ToUnicode(str));
+}
+
+#if _UNICODE
+static std::wstring Ansi2WChar(LPCSTR pszSrc, int nLen)
+{
+	int nSize = MultiByteToWideChar(CP_ACP, 0, (LPCSTR)pszSrc, nLen, 0, 0);
+	if (nSize <= 0) return NULL;
+
+	WCHAR *pwszDst = new WCHAR[nSize + 1];
+	if (NULL == pwszDst) return NULL;
+
+	MultiByteToWideChar(CP_ACP, 0, (LPCSTR)pszSrc, nLen, pwszDst, nSize);
+	pwszDst[nSize] = 0;
+
+	if (pwszDst[0] == 0xFEFF)                    // skip Oxfeff
+	for (int i = 0; i < nSize; i++)
+		pwszDst[i] = pwszDst[i + 1];
+
+	wstring wcharString(pwszDst);
+	delete pwszDst;
+
+	return wcharString;
+}
+
+static std::wstring s2ws(const string& s)
+{
+	return Ansi2WChar(s.c_str(), s.size());
+}
+static string WChar2Ansi(LPCWSTR pwszSrc)
+{
+	int nLen = WideCharToMultiByte(CP_ACP, 0, pwszSrc, -1, NULL, 0, NULL, NULL);
+
+	if (nLen <= 0) return std::string("");
+
+	char* pszDst = new char[nLen];
+	if (NULL == pszDst) return std::string("");
+
+	WideCharToMultiByte(CP_ACP, 0, pwszSrc, -1, pszDst, nLen, NULL, NULL);
+	pszDst[nLen - 1] = 0;
+
+	std::string strTemp(pszDst);
+	delete[] pszDst;
+
+	return strTemp;
+}
+#endif
